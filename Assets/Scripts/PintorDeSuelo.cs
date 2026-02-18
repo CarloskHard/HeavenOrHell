7bac878a9d6651780118ba8ddadc012ef26463c3
AmayaSan96 <68030609+AmayaSan96@users.noreply.github.com>
2026-02-18 14:32:40 +0100

    funciones para crear carriles hecha

--- Assets/Scripts/PintorDeSuelo.cs
+++ Assets/Scripts/PintorDeSuelo.cs
@@ -2,6 +2,15 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+
+public enum TipoCarril
+{
+    Vacio, // Un carril sin nada, solo asfalto
+    LineaContinua,
+    FlechaDerecha,
+    FlechaIzquierda
+    // Aquí podrías añadir más en el futuro: LineaDiscontinua, PasoDeCebra...
+}
 public class PintorDeSuelo : MonoBehaviour
 {
 
@@ -13,7 +22,8 @@ public class PintorDeSuelo : MonoBehaviour
 
     //Tomamos los tiles que vamos a usar de resources
     // 1. Solo declaramos las variables aquí
-    private TileBase tileFlecha;
+    private TileBase tileFlechaIzquierda;
+    private TileBase tileFlechaDerecha;
     private TileBase tileAsfalto;
     private TileBase tileLineaContinua;
     private TileBase tileMediana;
@@ -24,17 +34,22 @@ public class PintorDeSuelo : MonoBehaviour
     void Start()
     {
         // 2. Cargamos los recursos dentro de Start (o Awake)
-        tileFlecha = Resources.Load<TileBase>("Tiles/Flecha");
+        tileFlechaIzquierda = Resources.Load<TileBase>("Tiles/FlechaIzquierda");
+        tileFlechaDerecha = Resources.Load<TileBase>("Tiles/FlechaDerecha");
         tileAsfalto = Resources.Load<TileBase>("Tiles/Asfalto");
         tileLineaContinua = Resources.Load<TileBase>("Tiles/LineaContinua");
         tileMediana = Resources.Load<TileBase>("Tiles/Mediana");
         tilePasoZebra = Resources.Load<TileBase>("Tiles/PasoZebra");
 
-        //PintarAsfalto();
-        PintarCarretera1Carril(0);
+        //PintarCarriles();
+        PintarCarrilIzq(0);
         PintarMediana(1);
-        PintarCarretera1Carril(3);
-        PintarCarretera1Carril(4);
+        PintarCarrilIzq(3);
+        LineaContinua(4);
+        PintarCarrilDer(5);
+        LineaContinua(6);
+        PintarCarrilIzq(7);
+        PintarMediana(8);
 
     }
 
@@ -74,16 +89,57 @@ public class PintorDeSuelo : MonoBehaviour
 
     }
 
+    private void LineaContinua(int altura)
+    {
+        UnityEngine.Quaternion rotacion90 = UnityEngine.Quaternion.Euler(0, 0, 90);
+        UnityEngine.Quaternion rotacion270 = UnityEngine.Quaternion.Euler(0, 0, -90);
 
-    private void PintarCarretera1Carril(int altura)
+
+        Matrix4x4 matrix1 = Matrix4x4.TRS(Vector3.zero, rotacion90, Vector3.one);
+        Matrix4x4 matrix2 = Matrix4x4.TRS(Vector3.zero, rotacion270, Vector3.one);
+
+        for (int x = 0; x < anchoDeLaLinea; x++)
+        {
+
+            Vector3Int vector1 = new Vector3Int(x - 5, altura, 0);
+            marcasDeSueloTilemap.SetTile(vector1, tileLineaContinua);
+            
+            marcasDeSueloTilemap.SetTransformMatrix(vector1, matrix1);
+        }
+        //Paso de Zebra
+        Vector3Int vectorPasoZebra1 = new Vector3Int(-1, altura, 0);
+        Vector3Int vectorPasoZebra2 = new Vector3Int(0, altura, 0);
+
+        marcasDeSueloTilemap.SetTile(vectorPasoZebra1, tilePasoZebra);
+        marcasDeSueloTilemap.SetTile(vectorPasoZebra2, tilePasoZebra);
+
+
+        // 4. Aplicamos la matriz a la celda donde pusimos el paso de zebra
+        marcasDeSueloTilemap.SetTransformMatrix(vectorPasoZebra1, matrix1);
+        marcasDeSueloTilemap.SetTransformMatrix(vectorPasoZebra2, matrix2);
+
+    }
+
+    private void PintarCarrilIzq(int altura)
+    {
+        PintarCarril(tileFlechaIzquierda, altura);
+    }
+
+    private void PintarCarrilDer(int altura)
+    {
+        PintarCarril(tileFlechaDerecha, altura);
+    }
+
+
+    private void PintarCarril(TileBase spriteFlecha, int altura)
     {
         //Flechas
         Vector3Int vectorFlecha1 = new Vector3Int(-5, altura, 0);
         Vector3Int vectorFlecha2 = new Vector3Int(4, altura, 0);
 
 
-        marcasDeSueloTilemap.SetTile(vectorFlecha1, tileFlecha);
-        marcasDeSueloTilemap.SetTile(vectorFlecha2, tileFlecha);
+        marcasDeSueloTilemap.SetTile(vectorFlecha1, spriteFlecha);
+        marcasDeSueloTilemap.SetTile(vectorFlecha2, spriteFlecha);
 
         //Paso de Zebra
         Vector3Int vectorPasoZebra1 = new Vector3Int(-1, altura, 0);
@@ -99,7 +155,7 @@ public class PintorDeSuelo : MonoBehaviour
         Matrix4x4 matrix1 = Matrix4x4.TRS(Vector3.zero, rotacion90, Vector3.one);
         Matrix4x4 matrix2 = Matrix4x4.TRS(Vector3.zero, rotacion270, Vector3.one);
 
-        // 4. Aplicamos la matriz a la celda donde pusimos la flecha
+        // 4. Aplicamos la matriz a la celda donde pusimos el paso de zebra
         marcasDeSueloTilemap.SetTransformMatrix(vectorPasoZebra1, matrix1);
         marcasDeSueloTilemap.SetTransformMatrix(vectorPasoZebra2, matrix2);
 
